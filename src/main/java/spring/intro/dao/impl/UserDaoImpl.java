02e204f8c5139c692bc584b53694212656780d66
ana-dav <opa.super.op@gmail.com>
2020-06-02 17:29:07 +0300

    session

--- src/main/java/spring/intro/dao/impl/UserDaoImpl.java
+++ src/main/java/spring/intro/dao/impl/UserDaoImpl.java
@@ -1,23 +1,41 @@
 package spring.intro.dao.impl;
 
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import spring.intro.dao.interfaces.UserDao;
+import spring.intro.exception.DataProcessingException;
 import spring.intro.model.User;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
+    @Autowired
     private SessionFactory sessionFactory;
-
-    public UserDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("There was an error inserting "
+                    + user, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
